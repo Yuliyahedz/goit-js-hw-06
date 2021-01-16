3117bdc253cd9161747cd027d2672f2d3bfbfcf6
Yuliyahedz <juliagedz@gmail.com>
2021-01-16 13:13:00 +0200

    hw uploaded

---
+++ js/task-1.js
@@ -0,0 +1,6 @@
+const getUserNames = (users) => {
+  // твой код
+};
+
+console.log(getUserNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
